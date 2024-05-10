5b89d024a648d75c399fac97f34cc9295ea68047
Pablo Dornhege <dornhege@htw-berlin.de>
2024-05-10 11:00:20 +0200

    Add files via upload

---
+++ EventInvokationKeyCode.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventInvokationKeyCode : MonoBehaviour
+{
+    public bool EventInvokationKeyCodeActive = true;
+    public KeyCode ActivationKey;
+    [Space(5)]
+    public UnityEvent OnKeyDownEvent;
+    [Space(5)]
+    public UnityEvent OnKeyHoldEvent;
+    [Space(5)]
+    public UnityEvent OnKeyUpEvent;
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKeyDown(ActivationKey))
+        {
+            OnKeyDownEvent.Invoke();
+        }
+
+        if (Input.GetKey(ActivationKey))
+        {
+            OnKeyHoldEvent.Invoke();
+        }
+
+        if (Input.GetKeyUp(ActivationKey))
+        {
+            OnKeyUpEvent.Invoke();
+        }
+    }
+}
---
+++ EventInvokationTrigger.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class EventInvokationTrigger : MonoBehaviour
+{
+
+    public bool EventInvokationTriggerActive = true;
+
+    [Header("Leave empty detect all collisions")]
+    public string ColliderTag = null;
+
+    [Space(5)]
+    public UnityEvent OnTriggerEnterEvent;
+    [Space(5)]
+    public UnityEvent OnTriggerStayEvent;
+    [Space(5)]
+    public UnityEvent OnTriggerExitEvent;
+
+
+    private void OnTriggerEnter(Collider other)
+
+    {
+
+        if ((ColliderTag == "") || other.tag == (ColliderTag))
+        {
+            OnTriggerEnterEvent.Invoke();
+        }
+
+
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if ((ColliderTag == "") || other.tag == (ColliderTag))
+        {
+            OnTriggerStayEvent.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if ((ColliderTag == "") || other.tag == (ColliderTag))
+        {
+            OnTriggerExitEvent.Invoke();
+        }
+    }
+
+}
